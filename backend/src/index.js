688755cfc2759d33e1a14baf36e0b057220f878d
faukirijatul <faukirijatul42@gmail.com>
2026-03-07 15:02:29 +0700

    fix mongo db connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
 dotenv.config();
 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.MONGODB_URI);
-
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    serverSelectionTimeoutMS: 10000,
-    socketTimeoutMS: 45000,
-  })
-  .then(() => {
-    console.log('Database connected successfully');
-  })
-  .catch((err) => {
-    console.error('Database connection error:', err.message);
-    console.error('Full error:', err);
-  });
-
 import wordRouter from './routes/word.route.js';
+import connectDB from './database/database.js';
 app.use('/words', wordRouter);
 
 const PORT = process.env.PORT || 5000;
